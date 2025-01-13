7ca4a58da07916f1c11732b805a4d2be78a1ff23
Linoe <ps800307@gmail.com>
2025-01-13 11:19:06 +0800

    調整資源資料夾分類與添加人物數據
    專案變更以下
    - 將 Prefabs 與 Datas 資料夾依照 Character Enemy Weapon 分類
    - 添加 CharacterScriptable.cs 人物數據 (未使用)

---
+++ Assets/Scripts/Scriptables/CharacterScriptable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Character", order = 1)]
+public class CharacterScriptable : ScriptableObject
+{
+    public string Name; // 顯示名稱
+    public GameObject spritePrefab; // 資源欲置物
+    public WeaponScriptable startingWeapon; // 起始武器
+}
