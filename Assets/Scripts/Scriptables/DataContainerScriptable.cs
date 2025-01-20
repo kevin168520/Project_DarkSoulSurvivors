a8988875b070881c06de6152fd0f49aece6fa1c1
Linoe <ps800307@gmail.com>
2025-01-20 13:31:28 +0800

    添加跨場景資源 DataContainerScriptable

---
+++ Assets/Scripts/Scriptables/DataContainerScriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataContainer", menuName = "ScriptableObjects/DataContainer", order = 1)]
+public class DataContainerScriptable : ScriptableObject
+{
+    public CharacterScriptable character;
+}
