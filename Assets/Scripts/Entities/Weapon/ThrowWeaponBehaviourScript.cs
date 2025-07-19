7c88cf3fa24c16fa8dd8f0ec9f9e3f49b231a53f
Linoe <ps800307@gmail.com>
2025-07-19 18:19:10 +0800

    添加第三種武器

---
+++ Assets/Scripts/Entities/Weapon/ThrowWeaponBehaviourScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowWeaponBehaviourScript : WeaponBehaviourBase
+{
+    Vector3 velocity;
+    float gravity = -30f;
+    float verticalSpeed = 14f; // 初始向上速度
+    HashSet<Object> enemiesHit = new HashSet<Object>(); // 避免相同敵人多次攻擊 HashSet 無排序速度較快
+
+    // 開啟攻擊
+    protected override void OnAttackStart()
+    {
+        PlaySound(enAudioSfxData.PhysicalWeaponArrow);
+        velocity = new Vector3(0, verticalSpeed, 0);
+    }
+    
+    // 彈丸移動
+    override protected void OnUpdateStart() {
+        // 水平持續向前移動
+        transform.position += FlightDirection * FlightSpeed * Time.deltaTime;
+      
+        // 垂直方向拋物線運動
+        velocity.y += gravity * Time.deltaTime;
+        transform.position += new Vector3(0, velocity.y, 0) * Time.deltaTime;
+    }
+
+    // 執行攻擊
+    override protected void HandleAttack(Collider2D collision)
+    {
+        if(OnAttackEvent(collision)){
+          enemiesHit.Add(collision); // 加入曾被攻擊過
+        }
+    }
+
+    // 結束攻擊
+    protected override void OnAttackEnd()
+    {
+        Destroy(gameObject); // 彈丸自滅
+    }
+}
---
+++ Assets/Scripts/Entities/Weapon/ThrowWeaponScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowWeaponScript : WeaponHandlerBase
+{
+    IDirection direction => PlayerManager.PLAYER.move; // 方向組件
+    [SerializeField] private WeaponBehaviourBase projectilePrefab;
+
+    // 執行攻擊
+    override protected void HandleAttack()
+    {
+        StartCoroutine(AttackProcess());
+    }
+
+    // 攻擊過程 會有一次攻擊複數子彈
+    IEnumerator AttackProcess() {
+      SpawnProjectile();
+      yield return new WaitForSeconds(0.3f);
+    }
+
+    // 生成子彈物件
+    void SpawnProjectile() {
+      // 子彈場景狀態
+      WeaponBehaviourBase projectile = Instantiate(projectilePrefab);
+      projectile.transform.position = transform.position;
+      // projectile.transform.parent = transform;
+      projectile.gameObject.SetActive(true);
+      
+      // 子彈屬性
+      projectile.ApplyWeaponStats(this);
+      projectile.OnAttackEvent = ApplyDamage;
+      projectile.FlightDirection = direction.Normalized;
+
+      // 子彈方向
+      float angle = Mathf.Atan2(direction.Normalized.y, direction.Normalized.x) * Mathf.Rad2Deg;
+      projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
