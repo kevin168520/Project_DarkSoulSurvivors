6e296769d5263492950238afdccd32d56011236b
Linoe <ps800307@gmail.com>
2025-08-16 18:35:53 +0800

    掉落物組件增加掉落範圍

--- Assets/Scripts/Components/ItemDropComponent.cs
+++ Assets/Scripts/Components/ItemDropComponent.cs
@@ -6,14 +6,20 @@ public class ItemDropComponent : MonoBehaviour
 {
     public List<GameObject> dropItemPrefab; // 掉落物品
     [Range(0f, 1f)] public float chance; // 掉落率
+    public float range = 0.5f; // 掉落範圍
 
     // 執行機率掉落物品
     public void HandleDropItem()
     {
         if (Random.value < chance)
         {
+            Vector3 randomPos = new Vector3(
+                Random.Range(-range, range),
+                0,
+                Random.Range(-range, range)
+            );
             GameObject toDrop = dropItemPrefab[Random.Range(0, dropItemPrefab.Count)];
-            Transform t = Instantiate(toDrop).transform;
+            Transform t = Instantiate(toDrop, transform.position + randomPos, Quaternion.identity).transform;
             t.position = transform.position;
         }
     }
