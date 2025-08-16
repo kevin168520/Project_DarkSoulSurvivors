b5f73c8b3a419a961f849dbdbade4d600620b989
Linoe <ps800307@gmail.com>
2025-08-16 15:13:19 +0800

    重構排版

--- Assets/Scripts/Components/ItemDropComponent.cs
+++ Assets/Scripts/Components/ItemDropComponent.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class ItemDropComponent : MonoBehaviour
 {
-  public List<GameObject> dropItemPrefab; // 掉落物品
-  [Range(0f, 1f)] public float chance; // 掉落率
+    public List<GameObject> dropItemPrefab; // 掉落物品
+    [Range(0f, 1f)] public float chance; // 掉落率
 
-  // 執行機率掉落物品
-  public void HandleDropItem() {
-    if(Random.value < chance)
+    // 執行機率掉落物品
+    public void HandleDropItem()
     {
-      GameObject toDrop = dropItemPrefab[Random.Range(0, dropItemPrefab.Count)];
-      Transform t = Instantiate(toDrop).transform;
-      t.position = transform.position;
+        if (Random.value < chance)
+        {
+            GameObject toDrop = dropItemPrefab[Random.Range(0, dropItemPrefab.Count)];
+            Transform t = Instantiate(toDrop).transform;
+            t.position = transform.position;
+        }
     }
-  }
 }
