d49e08494580a653f78a5386fe8a70dbfadc0678
Linoe <ps800307@gmail.com>
2025-09-14 11:38:45 +0800

    重構 StageManager 排版

--- Assets/Scripts/Scenes/MainGame/StageManager.cs
+++ Assets/Scripts/Scenes/MainGame/StageManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : ManagerMonoBase
@@ -8,45 +6,50 @@ public class StageManager : ManagerMonoBase
 
     void Start()
     {
-      // 註冊 關卡時間 監聽
-      LevelTimerManager.AddListener(OnLevelTimer);
+        // 註冊 關卡時間 監聽
+        LevelTimerManager.AddListener(OnLevelTimer);
     }
 
+    // 關卡時間
     void OnLevelTimer(int time)
     {
         // 判定事件觸發
-        if(stageEvent.Check(time)) return;
-        
+        if (stageEvent.Check(time)) return;
+
         // 取得事件
         StageEvent e = stageEvent.Current();
 
         // 事件執行
         OnLevelEvent(e);
-        
+
         // 事件進展
         stageEvent.Next();
     }
 
+    // 關卡事件
     void OnLevelEvent(StageEvent stageEvent)
     {
-        switch(stageEvent.type){
-          case StageEventType.SpawnEnemy:
-            SpawnEnemy(stageEvent);
-            break;
-          case StageEventType.WinStage:
-            WinStage();
-            break;
+        switch (stageEvent.type)
+        {
+            case StageEventType.SpawnEnemy:
+                SpawnEnemy(stageEvent);
+                break;
+            case StageEventType.WinStage:
+                WinStage();
+                break;
         }
     }
-    
+
     // 生成怪物群
-    void SpawnEnemy(StageEvent stageEvent){
+    void SpawnEnemy(StageEvent stageEvent)
+    {
         EnemyManager.AddEnemyWave(stageEvent); // 關卡生成敵人群添加到敵人管理者
-        if(stageEvent.message.Length != 0) Debug.Log(stageEvent.message);
+        if (stageEvent.message.Length != 0) Debug.Log(stageEvent.message);
     }
-    
+
     // 關卡通關
-    void WinStage(){
-      GameManager.GameComplete();
+    void WinStage()
+    {
+        GameManager.GameComplete();
     }
 }
