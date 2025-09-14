12191f5b2ccc6c279794f0a1f5c4179cc81ed0ed
Linoe <ps800307@gmail.com>
2025-09-14 11:26:49 +0800

    重構 LevelTimerManager

--- Assets/Scripts/Scenes/MainGame/LevelTimerManager.cs
+++ Assets/Scripts/Scenes/MainGame/LevelTimerManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,30 +5,32 @@ using UnityEngine.Events;
 /// <summary> 負責關卡時間與畫面時間 </summary>
 public class LevelTimerManager : ManagerMonoBase
 {
-  [Header("關卡時間UI")]
-  [SerializeField] TextMeshProUGUI _LevelTimerText;
+    [Header("關卡時間UI")]
+    [SerializeField]
+    TextMeshProUGUI _LevelTimerText;
 
-  [Header("關卡時間")]
-  [SerializeField] float stageTime;
-  int sec;
-  /// <summary> 關卡時間 秒數 </summary>
-  public int Sec => sec;
+    [Header("關卡時間")]
+    [SerializeField]
+    float stageTime;
 
-  UnityEvent<int> _listen = new UnityEvent<int>();
-  /// <summary> 關卡時間監聽 每秒觸發一次 </summary>
-  public void AddListener(UnityAction<int> callback) => _listen.AddListener(callback);
+    /// <summary> 關卡時間 秒數 </summary>
+    public int Sec { get; private set; }
 
-  void Update()
-  {
-    stageTime += Time.deltaTime;
-    // 每秒觸發一次
-    if (sec <= stageTime)
+    /// <summary> 關卡時間監聽 每秒觸發一次 </summary>
+    UnityEvent<int> _listen = new UnityEvent<int>();
+    public void AddListener(UnityAction<int> callback) => _listen.AddListener(callback);
+
+    void Update()
     {
-      sec = Mathf.CeilToInt(stageTime);
-      _LevelTimerText.text = FormatLevelTimerText(sec / 60, sec % 60);
-      _listen.Invoke(sec);
+        stageTime += Time.deltaTime;
+        // 每秒觸發一次
+        if (Sec <= stageTime)
+        {
+            Sec = Mathf.CeilToInt(stageTime);
+            _LevelTimerText.text = FormatLevelTimerText(Sec / 60, Sec % 60);
+            _listen.Invoke(Sec);
+        }
     }
-  }
 
-  string FormatLevelTimerText(int m, int s) => $"{m:00}：{s:00}";
+    string FormatLevelTimerText(int m, int s) => $"{m:00}：{s:00}";
 }
