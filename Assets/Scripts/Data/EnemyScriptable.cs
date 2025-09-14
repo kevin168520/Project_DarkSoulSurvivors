36bdb13d462f7d15da1c780f0ce5f7fba71c45fd
Linoe <ps800307@gmail.com>
2025-09-14 12:19:50 +0800

    修改 EnemyScriptable 新增資源成員

--- Assets/Scripts/Data/EnemyScriptable.cs
+++ Assets/Scripts/Data/EnemyScriptable.cs
@@ -1,15 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemy", order = 1)]
 public class EnemyScriptable : ScriptableObject
 {
-    public GameObject spritePrefab; // 資源欲置物
+    [Title("基礎數值")]
     public string showName;
-
     public int hp;
     public int damage;
     public float speed;
+
+    [Title("參考資源")]
+    public Sprite sprite;
+    public RuntimeAnimatorController animator;
+    public Vector2 offset;
+    public Vector2 size;
+
+    [Title("掉落物品")]
+    [AssetsOnly, AssetSelector(Paths = "Assets/Prefabs/Item", FlattenTreeView = true)]
+    public List<GameObject> drop;
 }
