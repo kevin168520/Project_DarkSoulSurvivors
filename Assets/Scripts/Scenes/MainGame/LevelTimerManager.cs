b4db59f949ba59bb0dd411155b4648059c285bb5
Linoe <ps800307@gmail.com>
2025-09-14 11:36:44 +0800

    修改 LevelTimerManager 關卡時間觸發，改為發送事件
    修改如下:
    LevelTimerEvent.cs 負責關卡時間事件
    LevelTimerManager.cs 移除事件成員 改為發送 LevelTimerEvent

--- Assets/Scripts/Scenes/MainGame/LevelTimerManager.cs
+++ Assets/Scripts/Scenes/MainGame/LevelTimerManager.cs
@@ -2,7 +2,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary> 負責關卡時間與畫面時間 </summary>
+/// <summary> 負責關卡時間與畫面時間 透過 LevelTimerEvent 發送</summary>
 public class LevelTimerManager : ManagerMonoBase
 {
     [Header("關卡時間UI")]
@@ -16,10 +16,6 @@ public class LevelTimerManager : ManagerMonoBase
     /// <summary> 關卡時間 秒數 </summary>
     public int Sec { get; private set; }
 
-    /// <summary> 關卡時間監聽 每秒觸發一次 </summary>
-    UnityEvent<int> _listen = new UnityEvent<int>();
-    public void AddListener(UnityAction<int> callback) => _listen.AddListener(callback);
-
     void Update()
     {
         stageTime += Time.deltaTime;
@@ -28,7 +24,7 @@ public class LevelTimerManager : ManagerMonoBase
         {
             Sec = Mathf.CeilToInt(stageTime);
             _LevelTimerText.text = FormatLevelTimerText(Sec / 60, Sec % 60);
-            _listen.Invoke(Sec);
+            EventGlobalManager.InvokeEvent(new LevelTimerEvent() { time = Sec });
         }
     }
 
