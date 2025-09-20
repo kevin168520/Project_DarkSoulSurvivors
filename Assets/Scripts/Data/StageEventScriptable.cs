ad83767952717b2d88703c0305b305cd78a9a75c
Linoe <ps800307@gmail.com>
2025-09-20 11:21:58 +0800

    將 StageEvent 獨立成檔案

--- Assets/Scripts/Data/StageEventScriptable.cs
+++ Assets/Scripts/Data/StageEventScriptable.cs
@@ -3,13 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum StageEventType
-{
-    SpawnEnemy,
-    WinStage,
-    StartStage
-}
-
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StageEvent", order = 1)]
 public class StageEventScriptable : ScriptableObject, IEnumerable<StageEvent>
 {
@@ -38,24 +31,4 @@ public class StageEventScriptable : ScriptableObject, IEnumerable<StageEvent>
     // 用於 foreach 循序
     public IEnumerator<StageEvent> GetEnumerator() => table.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => table.GetEnumerator();
-}
-
-[Serializable]
-public class StageEvent
-{
-    public StageEventType type = StageEventType.SpawnEnemy;
-    [Range(0, 1800)] public float time;     // 出現時間
-    public string message;                  // 後台訊息
-    public EnemyScriptable enemy;           // 敵人數據
-    [Range(0, 200)] public int enemyCount;  // 敵人數量
-
-    public StageEvent Clone()
-    {
-        StageEvent c = new StageEvent();
-        c.time = this.time;
-        c.message = this.message;
-        c.enemy = this.enemy;
-        c.enemyCount = this.enemyCount;
-        return c;
-    }
 }
